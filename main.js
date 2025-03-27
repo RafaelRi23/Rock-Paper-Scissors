ad6ec5830f9480889ee8817a7f335b0ef30db7cb
Rafael Ribeiro <ribeirorafael8814@gmail.com>
2025-03-27 00:11:05 -0300

    Add loop to play 5 rounds and polish the conditionals to make the code more clean

--- main.js
+++ main.js
@@ -1,67 +1,59 @@
 // Function to get the computer choice
 function getComputerChoice () {
-// variable to get a random number
-   let random = Math.random()
-// variable to capture the number
-   let computerChoice
-//    conditional to decide which choice will be
-    if (random >= 0.7) {
-        computerChoice = `Pedra`
-        console.log(computerChoice)
-        
-    } else if (random >= 0.4) {
-        computerChoice = `Papel`
-        console.log(computerChoice)
-        
-    } else {
-        computerChoice = `Tesoura`
-        console.log (computerChoice)
-        
-    }
+    // variable to get a random number
+    const random = Math.random();
+    // variable to capture the number
+    let computerChoice;
+    // conditional ternary operator to decide which choice will be
+    computerChoice = random < 0.34 ? `Pedra` : random < 0.67 ? `Papel` : `Tesoura`
+    console.log(computerChoice)
+    
     return computerChoice
 }
 
 // Function to get the human choice
 function getHumanChoice () {
-    let humanAnswer = prompt(`Pedra, Papel ou Tesoura?`)
-    console.log(humanAnswer)
-    // return the choice with the first letter in upper case
-    return humanAnswer.charAt(0).toUpperCase() + humanAnswer.slice(1)
-    // need to think a way to lower case all letters if the prompt is all upper case
+    // variable to capture the choice with prompt
+    let humanChoice = prompt(`Pedra, Papel ou Tesoura?`)
+    // convert all letters to lower case
+    humanChoice = humanChoice.toLowerCase();
+    // convert the first letter to upper case
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+    console.log(humanChoice)
+
+    return humanChoice
 }
-// reference error prompt is not defined, but the code is running, need to solve this later
+
+// the error `reference error prompt` occurs in node because it doesn`t have prompt, but in browser this error doesn`t exist
 
 // variable to count when human wins
 let humanScore = 0
 // variable to count when computer wins
 let computerScore = 0
 
+
+   
 // function to play a round
 function playRound (humanChoice, computerChoice) {
-    if (humanChoice == `Pedra` && computerChoice == `Tesoura` ) {
-        console.log(`Voce venceu! Sua pontuacao eh ` + humanScore)
-    } else if (humanChoice == `Pedra` && computerChoice == `Pedra`) {
-        console.log(`Empate`)
-    } else if (humanChoice == `Pedra` && computerChoice == `Papel`) {
-        console.log(`Voce perdeu`)
-    } else if (humanChoice == `Papel` && computerChoice == `Tesoura` ) {
-        console.log(`Voce perdeu`)
-    } else if (humanChoice == `Papel` && computerChoice == `Pedra`) {
-        console.log(`Voce venceu! Sua pontuacao eh ` + humanScore)
-    } else if (humanChoice == `Papel` && computerChoice == `Papel`) {
-        console.log(`Empate`)
-    } else if (humanChoice == `Tesoura` && computerChoice == `Tesoura` ) {
-        console.log(`Empate`)
-    } else if (humanChoice == `Tesoura` && computerChoice == `Pedra`) {
-        console.log(`Voce perdeu`)
-    } else 
-        console.log(`Voce venceu! Sua pontuacao eh ` + humanScore)
+
+    if ((humanChoice === `Pedra` && computerChoice === `Tesoura`) || (humanChoice === `Papel` && computerChoice === `Pedra`) || (humanChoice === `Tesoura` && computerChoice === `Papel`)) {
+        humanScore++
+        console.log(`Voce venceu! Sua pontuacao eh ` + humanScore + `. A pontuacao do computador eh ` + computerScore)
+    } else if (humanChoice === computerChoice) {
+        console.log(`Empate. Sua pontuacao eh ` + humanScore + ` e a pontuacao do computador eh ` + computerScore)
+    } else {
+        computerScore++
+        console.log(`Voce perdeu. Sua pontuacao eh ` + humanScore + `. A pontuacao do computador eh ` + computerScore)
+    }
 }
-// need to think a way to polish this conditional
 
-// variable to initiate the human choice
-const humanSelection = getHumanChoice()
-// variable to initiate the computer choice
-const computerSelection = getComputerChoice()
 
-playRound(humanSelection, computerSelection)
+for (let i = 0; i < 5; i++) {
+    // variable to initiate the human choice
+    const humanSelection = getHumanChoice()
+    // variable to initiate the computer choice
+    const computerSelection = getComputerChoice()
+
+    // run the function with the choices value
+    playRound(humanSelection, computerSelection)
+}
